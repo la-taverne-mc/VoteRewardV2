34c0054d953a677508873746f8c9b2fc3c2b43a8
syukoGit <syukodev@outlook.com>
2022-02-23 10:44:11 +0100

    Apply code style rules on the classes of the package "fr.lataverne.votereward"
    Signed-off-by: syukoGit <syukodev@outlook.com>

--- src/main/java/fr/lataverne/votereward/Constant.java
+++ src/main/java/fr/lataverne/votereward/Constant.java
@@ -1,11 +1,11 @@
 package fr.lataverne.votereward;
 
 public enum Constant {
-	;
+    ;
 
-	public static final int MAX_NB_REWARDS_RETRIEVING = 35;
+    public static final int EXPIRATION_TIME = 15;
 
-	public static final int EXPIRATION_TIME = 15;
+    public static final int MAX_NB_REWARDS_RETRIEVING = 35;
 
-	public static final int PLAYER_INVENTORY_SIZE = 36;
+    public static final int PLAYER_INVENTORY_SIZE = 36;
 }
